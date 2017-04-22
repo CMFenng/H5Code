c1c331029e453b7c3b4cf3fc23d0942fd35b30fe
mingfengst <mingfengst@163.com>
2017-04-22 21:53:42 +0800

    Commit by CMF

--- D03_Vue/lesson/notes/src/App.vue
+++ D03_Vue/lesson/notes/src/App.vue
@@ -1,23 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <toolbar></toolbar>
+        <note-list></note-list>
+        <editor></editor>
+    </div>
 </template>
 
-<script>
+<script type="text/javascript">
+// 引入组件文件
+import Toolbar from './components/Toolbar'
+import NoteList from './components/NoteList'
+import Editor from './components/Editor'
+
 export default {
-  name: 'app'
+    components : {
+        Toolbar,
+        NoteList,
+        Editor
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style type="text/css">
+html,#app{
+    height: 100%;
+}
+
+body{
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 100%;
+    max-height: 100%;
+    position: relative;
 }
 </style>
---
+++ D03_Vue/lesson/notes/src/components/Toolbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="toolbar">
+        <i @click="addOne"class="glyphicon glyphicon-plus"></i>
+        <i @click="toggleFavorite" class="glyphicon glyphicon-star"
+            v-bind:class="{starred : activeNote.favorite}"></i>
+        <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+    computed : {
+        notes(){
+            return this.$store.getters.notes
+        },
+        activeNote(){
+            return this.$store.getters.activeNote;
+        }
+    },
+    methods : {
+        addOne(){
+            // 通过 dispatch 分发到 actions 中的 addNote
+            this.$store.dispatch('addNote');
+        },
+        toggleFavorite(){
+            this.$store.dispatch('toggleFavorite');
+        },
+        deleteNote(){
+            this.$store.dispatch('deleteNote');
+        }
+    }
+}
+</script>
+
+<style type="text/css">
+#toolbar {
+    float: left;
+    width: 80px;
+    height: 100%;
+    background-color: #30414D;
+    color: #767676;
+    padding: 35px 25px 25px 25px;
+}
+
+#toolbar i {
+    font-size: 30px;
+    margin-bottom: 35px;
+    cursor: pointer;
+    opacity: 0.8;
+    transition: opacity 0.5s ease;
+}
+
+#toolbar i:hover {
+    opacity: 1;
+}
+
+.starred {
+    color : #F7AE4F;
+}
+</style>
