20e149ed9b7845a667e89c534e3d6b2d3137e6d0
mingfengst <mingfengst@163.com>
2017-04-25 18:59:45 +0800

    Commit by CMF

---
+++ D05_Bootstrap/lesson/store/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+/* 导入 css 文件 */
+@import "assets/css/bootstrap.min.css";
+
+</style>
