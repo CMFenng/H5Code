fca6e6b7eb31845acdc6ef981a4286f25d9f5b7c
mingfengst <mingfengst@163.com>
2017-05-06 23:02:02 +0800

    Commit by CMF

--- D09_微信小程序andWebSocket/lesson/豆瓣电影_小程序/utils/util.js
+++ D09_微信小程序andWebSocket/lesson/豆瓣电影_小程序/utils/util.js
@@ -1,4 +1,20 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
 
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
 
 // 评分格式化
 function formatAverage(n) {
