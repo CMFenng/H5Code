1b408f885ace078b016cdc5dfe8e4fe846fb3954
mingfengst <mingfengst@163.com>
2017-05-21 23:10:06 +0800

    Commit by CMF

--- D02_Vue/lesson/src/components/tangcaiye.vue
+++ D02_Vue/lesson/src/components/tangcaiye.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrap">
         <h1>你好，{{ user }}</h1>
-        <button @click="showUser">点击弹出</button>
+        <!--<button @click="showUser">点击弹出</button>-->
         <button @click="getParams">获取参数</button>
     </div>
 </template>
